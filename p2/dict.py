2a80c39820ac90e82dcefb9477036cae955b3dcb
kskomi <kskomi196@gmail.com>
2023-12-26 18:44:02 +0300

    add dict.py

---
+++ p2/dict.py
@@ -0,0 +1,9 @@
+D = {'food': 'Apple', 'quantity': 4, 'color': 'Red'}
+print(D['food'])
+D['quantity'] += 10
+print(D['quantity'])
+dp = {}
+
+dp['name'] = input('Enter your name:')
+dp['age'] = int(input('Ener your age:'))
+print(dp)
