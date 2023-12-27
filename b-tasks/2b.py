f09e941ed8c2f356268a6062f3856c61dafc98c6
kskomi <kskomi196@gmail.com>
2023-12-27 16:20:57 +0300

    add 2b.py

---
+++ b-tasks/2b.py
@@ -0,0 +1,7 @@
+a = ['cheeze','sausage','potato','carrot','eggs','oysters','milk','bread','butter','meat']
+b = [12.2,25.3,7.19,8.23,12.29,98.1,6.3,4.2,15.3,27.92]
+c = [123,124,512,522,321,111,423,521,612,777]
+
+newlist = zip(a,b,c)
+for i in newlist:
+    print(i)
